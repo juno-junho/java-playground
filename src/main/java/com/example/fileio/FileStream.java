79e4441c13d127ed67116270817a9c9cc874c7a1
juno-junho <ssmm0205@naver.com>
2022-01-07 15:32:24 +0900

    FileStream test

--- src/main/java/com/example/fileio/FileStream.java
+++ src/main/java/com/example/fileio/FileStream.java
@@ -1,9 +1,21 @@
 package fileio;
 
+import java.io.File;
+import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 public class FileStream {
     public static void main(String[] args) {
-        Files.list("./")
+        Path path = Paths.get("./fileio/JavaTest.java");
+
+        try {
+            Stream<String> stringStream =  Files.lines(path);
+            stringStream.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
