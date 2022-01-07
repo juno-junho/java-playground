25a027abe042e5c133094fe061fa67892169c847
juno-junho <ssmm0205@naver.com>
2022-01-07 17:12:36 +0900

    using peek function in stream

---
+++ src/main/java/com/example/fileio/MapStreamTest.java
@@ -0,0 +1,34 @@
+package fileio;
+
+import java.io.File;
+import java.util.stream.Stream;
+
+public class MapStreamTest {
+    public static void main(String[] args) {
+        File[] fileArr = {new File("./fileio/FileStream.java"),
+                new File("./fileio/JavaTest.java"),
+                new File("./fileio/JavaTest.java"),
+                new File("./fileio/FileStream.md"),
+                new File("./fileio/InterateStream.java"),
+                new File("./fileio/RandomStream.java")
+        };
+
+        Stream<File> fileStream = Stream.of(fileArr);
+
+        //map()으로 Stream<File>을 Stream<String>으로 변환
+        Stream<String> fileNameStream = fileStream.map(File::getName);
+        fileNameStream.forEach(System.out::println);
+
+        fileStream = Stream.of(fileArr);
+
+        fileStream.map(File::getName)
+                .filter(s -> s.indexOf('.') != -1)  //확장자 없는 것 제외
+                .peek(s -> System.out.printf("filename=%s%n",s))    //중간에 확인할수 있음. debugging 용도
+                .map(s -> s.substring(s.indexOf('.') + 1))  //확장자만 추출
+                .peek(s -> System.out.printf("extension=%s%n",s))
+                .map(String::toUpperCase)   //대문자로변환
+                .distinct()
+                .forEach(System.out::println);
+        System.out.println();
+    }
+}
---
+++ src/main/java/com/example/fileio/StreamSorting.java
@@ -0,0 +1,65 @@
+package fileio;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.stream.Stream;
+
+public class StreamSorting {
+    public static void main(String[] args) {
+        Stream<Student> studentStream = Arrays.stream(new Student[]{
+                new Student("lee", 3, 300),
+                new Student("kim", 1, 200),
+                new Student("Ann", 2, 100),
+                new Student("park", 2, 150),
+                new Student("So", 1, 200),
+                new Student("Na", 3, 290),
+                new Student("Lim", 3, 180)
+        });
+
+//        studentStream.sorted(Comparator.comparing(Student::getBan)  // 1. 반별정렬 sort with class(ban)
+        studentStream.sorted(Comparator.comparing((Student s) -> s.getBan())    // 이렇게 메소드 참조를 람다식으로 바꿀수 있음.
+                .reversed()
+                .thenComparing(Comparator.naturalOrder())
+                .reversed())  // 2. 기본정렬 (compareTo overriding)
+                .forEach(System.out::println);
+    }
+}
+
+class Student implements Comparable<Student>{
+    String name;
+    int ban;
+    int totalScore;
+
+    public Student(String name, int ban, int totalScore) {
+        this.name = name;
+        this.ban = ban;
+        this.totalScore = totalScore;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", ban=" + ban +
+                ", totalScore=" + totalScore +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBan() {
+        return ban;
+    }
+
+    public int getTotalScore() {
+        return totalScore;
+    }
+
+    // 총점 내림차순을 기본 정렬로 한다.
+    @Override
+    public int compareTo(Student o) {
+        return o.totalScore - this.totalScore;
+    }
+}
