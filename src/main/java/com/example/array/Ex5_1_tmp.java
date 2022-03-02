bc8ea360dc9117f2de226f4fa77566d90cfcc77e
juno-junho <ssmm0205@naver.com>
2022-03-02 16:05:03 +0900

    Creating array

---
+++ src/main/java/com/example/array/Ex5_1_tmp.java
@@ -0,0 +1,21 @@
+package array;
+
+public class Ex5_1_tmp {
+    public static void main(String[] args) {
+        int[] score;    // 1. 배열 score 선언(참조변수)
+        score = new int[5]; // 2. 배열의 생성(int 저장공간 x 5)
+
+        int[] score1 = new int[5]; // 배열의 선언과 생성을 동시에.
+        score1[3] = 100;
+
+        System.out.println("score[0] = " + score1[0]);
+        System.out.println("score[1] = " + score1[1]);
+        System.out.println("score[2] = " + score1[2]);
+        System.out.println("score[3] = " + score1[3]);
+        System.out.println("score[4] = " + score1[4]);
+
+        int value = 100;
+        System.out.println("value = " + value);
+
+    }
+}
