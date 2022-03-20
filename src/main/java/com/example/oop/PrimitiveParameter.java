bdea56e03073106956657e86a22917168db0839b
juno-junho <ssmm0205@naver.com>
2022-03-20 23:55:03 +0900

    Primitive parameter & Reference Parameter

--- src/main/java/com/example/oop/PrimitiveParameter.java
+++ src/main/java/com/example/oop/PrimitiveParameter.java
@@ -1,4 +1,21 @@
 package oop;
 
-public class PrimitiveParameter {
+class Data{
+    int x;
+}
+// 기본명 매개변수 (Primitive parameter)는 값만 복사. 객체가 어디있는지 모름.
+public class PrimitiveParameter  {
+    public static void main(String[] args) {
+        Data d = new Data();
+        d.x = 10;
+        System.out.println("main() : x = " + d.x);  // 10
+
+        change(d.x);    //  1000
+        System.out.println("After change(d.x)");
+        System.out.println("main() : x = " + d.x); // 10
+    }
+    static void change(int x){ // Primitive Parameter : read only. cannot change the value.
+        x = 1000;
+        System.out.println("change() : x = " + x);
+    }
 }
--- src/main/java/com/example/oop/ReferenceParameter.java
+++ src/main/java/com/example/oop/ReferenceParameter.java
@@ -1,5 +1,21 @@
 package oop;
 
+class Data2{
+    int x;
+}
+// 참조형 매개변수 (Reference Paramter)의 경우 참조변수가 객체의 주소값을 가지고 있으므로 읽고 쓰기가 가능.
 public class ReferenceParameter {
-    int add (int a, int b){ return a+b;}
+    public static void main(String[] args) {
+        Data2 d = new Data2();
+        d.x = 10;
+        System.out.println("main() : x = " + d.x);  // 10
+
+        change(d);    //  1000 -> 객체 주소를 가지고 있기 때문에 읽고 쓰기가 가능하다.
+        System.out.println("After change(d.x)");
+        System.out.println("main() : x = " + d.x); // 1000
+    }
+    static void change(Data2 d){
+        d.x = 1000;
+        System.out.println("change() : x = " + d.x);
+    }
 }
